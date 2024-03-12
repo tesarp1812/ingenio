36c69ae70007f2efcc27dc581b7ede35d6006619
Mac <mac@Macs-MacBook-Pro.local>
2024-03-12 14:44:01 +0700

    fix logincontroller

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GudangController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\loginController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
